759357ccef687eebc06609d76400d18c556e6d84
Darya-Kazharnovich <darya.kazharnovich@gmail.com>
2022-02-06 01:57:08 +0300

    feat: add reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let numberAbsoluteValue = Math.abs (n);
+    let numberSting = numberAbsoluteValue.toString();
+    let result = '';
+    let numberLength = numberSting.length;
+    let i = 0;
+    while (i < numberLength) {
+        result = numberSting[i] + result;
+        i = i + 1;
+    }
+    return result;
 }
